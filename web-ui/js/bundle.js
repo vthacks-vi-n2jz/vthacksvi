d0d2fd573302c370756af4f315ad21eeb25cfbaf
Nick Clark <nicholaus.clark115@gmail.com>
2019-02-16 12:39:58 -0500

    Login/Logout

--- web-ui/js/bundle.js
+++ web-ui/js/bundle.js
@@ -7835,10 +7835,46 @@ exports.SourceNode = require('./lib/source-node').SourceNode;
 },{"./lib/source-map-consumer":38,"./lib/source-map-generator":39,"./lib/source-node":40}],43:[function(require,module,exports){
 require('handlebars');
 
+function getCookie(c_name) {
+    let c_value = " " + document.cookie;
+    let c_start = c_value.indexOf(" " + c_name + "=");
+    if (c_start == -1) {
+        c_value = null;
+    }
+    else {
+        c_start = c_value.indexOf("=", c_start) + 1;
+        let c_end = c_value.indexOf(";", c_start);
+        if (c_end == -1) {
+            c_end = c_value.length;
+        }
+        c_value = unescape(c_value.substring(c_start,c_end));
+    }
+    return c_value;
+}
+
 $(document).ready(function() {
     $('.login-modal-open').click(function() {
         console.log('Opening Login Modal');
-        $('#login-modal').modal('toggle')
-    })
-})
+        $('#login-modal').modal('toggle');
+    });
+
+    $('#login-button').click(function() {
+        console.log('Logging In...');
+        document.cookie = 'logged=true';
+        window.location.href = 'transactions.html';
+    });
+
+    $('.logout-action').click(function() {
+        document.cookie = 'logged=false';
+        window.location.href = 'index.html';
+    });
+
+    if (getCookie('logged') === 'true') {
+        $('.login-link').hide();
+        $('.logout-link').show();
+    } else {
+        $('.login-link').show();
+        $('.logout-link').hide();
+    }
+});
 },{"handlebars":31}]},{},[43]);
